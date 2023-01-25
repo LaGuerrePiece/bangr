1402d47eb5d23d34dee4470f2e8a9e58d08a2418
0xturboblitz <florent.tavernier@gmail.com>
2023-01-25 16:58:33 +0100

    Suggest a vault button

--- src/screens/home/Invest.tsx
+++ src/screens/home/Invest.tsx
@@ -1,4 +1,13 @@
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  useColorScheme,
+  Linking,
+} from "react-native";
 import Vault from "../../components/Vault";
 import useVaultsStore from "../../state/vaults";
 
@@ -8,16 +17,17 @@ const averageApy = (apys: number[]) => {
 
 const Invest = () => {
   const vaults = useVaultsStore((state) => state.vaults);
+  const colorScheme = useColorScheme();
 
   return (
     <SafeAreaView className="top-20 h-[90%]">
-      <View className="">
+      <View>
         <Text className="text-5xl font-bold text-typo-light dark:text-typo-dark">
           Invest
         </Text>
       </View>
       <View className="h-[90%]">
-        <ScrollView className="">
+        <ScrollView>
           {vaults &&
             vaults
               .filter(
@@ -25,6 +35,36 @@ const Invest = () => {
                   vault.status === "active" || vault.status === "preview"
               )
               .map((vault) => <Vault key={vault.name} vault={vault} />)}
+          <View className="m-auto mt-1 mb-3 w-full rounded-lg bg-secondary-light p-2 shadow-sm dark:bg-secondary-dark">
+            <TouchableOpacity
+              onPress={() => Linking.openURL("https://tally.so/r/w2jYLb")}
+            >
+              <View className="flex-row items-center justify-between">
+                <View className="flex-row items-center">
+                  <Image
+                    className="h-6 w-6"
+                    // className="h-[16px] w-[24px]"
+                    source={
+                      colorScheme === "light"
+                        ? require("../../../assets/idea.png")
+                        : require("../../../assets/idea_white.png")
+                    }
+                  />
+                  <Text className="text-xl font-bold text-typo-light dark:text-typo-dark">
+                    Suggest a vault
+                  </Text>
+                </View>
+                <Image
+                  className="h-[16px] w-[24px]"
+                  source={
+                    colorScheme === "light"
+                      ? require("../../../assets/arrowright.png")
+                      : require("../../../assets/arrowrightwhite.png")
+                  }
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </View>
     </SafeAreaView>
