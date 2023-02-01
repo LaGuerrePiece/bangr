910e48f467efa22d148d0be1127483a7ba39caff
0xturboblitz <florent.tavernier@gmail.com>
2023-02-01 22:12:48 +0100

    NavBar refactor

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,23 +11,15 @@ import {
 const getTabImage = (tab: string, selected: string) => {
   switch (tab) {
     case "Swap":
-      if (tab === selected)
-        return (
-          <View>
-            <Image
-              className="mx-auto my-3 h-7 w-7"
-              source={require("../../assets/swapbtn-selected.png")}
-            />
-            <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
-              Swap
-            </Text>
-          </View>
-        );
       return (
         <View>
           <Image
             className="mx-auto my-3 h-7 w-7"
-            source={require("../../assets/swapbtn.png")}
+            source={
+              tab === selected
+                ? require("../../assets/swapbtn-selected.png")
+                : require("../../assets/swapbtn.png")
+            }
           />
           <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
             Swap
@@ -36,57 +28,38 @@ const getTabImage = (tab: string, selected: string) => {
       );
 
     case "Invest":
-      if (tab === selected)
-        return (
-          <View>
-            <Image
-              className="mx-auto my-3 h-7 w-7"
-              source={require("../../assets/vaultbtn-selected.png")}
-            />
-            <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
-              Invest
-            </Text>
-          </View>
-        );
       return (
         <View>
           <Image
             className="mx-auto my-3 h-7 w-7"
-            source={require("../../assets/vaultbtn.png")}
+            source={
+              tab === selected
+                ? require("../../assets/vaultbtn-selected.png")
+                : require("../../assets/vaultbtn.png")
+            }
           />
-          <Text className="-mt-2 mb-2 text-center font-bold text-typo-light dark:text-typo-dark">
+          <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
             Invest
           </Text>
         </View>
       );
 
     case "Wallet":
-      if (tab === selected)
-        return (
-          <View className="mx-auto">
-            <Image
-              className="my-3 ml-2 h-7 w-7"
-              source={require("../../assets/poche-selected.png")}
-            />
-            <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
-              Poche
-            </Text>
-          </View>
-        );
       return (
         <View className="mx-auto">
           <Image
             className="my-3 ml-2 h-7 w-7"
-            source={require("../../assets/poche.png")}
+            source={
+              tab === selected
+                ? require("../../assets/poche-selected.png")
+                : require("../../assets/poche.png")
+            }
           />
-          <Text className="-mt-2 mb-2 text-center font-bold text-typo-light dark:text-typo-dark">
+          <Text className="mb-2 -mt-2 text-center font-bold text-typo-light dark:text-typo-dark">
             Poche
           </Text>
         </View>
       );
-
-    default:
-      break;
   }
 };
 
@@ -97,21 +70,21 @@ interface NavBar {
 
 export const NavBar = ({ tab, setTab }: NavBar) => {
   return (
-    <View className="mt-14 flex flex-row bg-secondary-light dark:bg-secondary-dark">
+    <View className="mt-14 flex-row bg-secondary-light dark:bg-secondary-dark">
       <TouchableOpacity
-        className="m-auto w-1/3 "
+        className="m-auto w-1/3"
         onPress={() => setTab("Wallet")}
       >
-        <View className="">{getTabImage("Wallet", tab)}</View>
+        {getTabImage("Wallet", tab)}
       </TouchableOpacity>
       <TouchableOpacity className="m-auto w-1/3" onPress={() => setTab("Swap")}>
-        <View className="">{getTabImage("Swap", tab)}</View>
+        {getTabImage("Swap", tab)}
       </TouchableOpacity>
       <TouchableOpacity
         className="m-auto w-1/3 "
         onPress={() => setTab("Invest")}
       >
-        <View className="">{getTabImage("Invest", tab)}</View>
+        {getTabImage("Invest", tab)}
       </TouchableOpacity>
     </View>
   );
