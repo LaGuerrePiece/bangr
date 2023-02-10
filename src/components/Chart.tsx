7725f5b02b42a214eb10d90d6f0384ec6ae9eb1e
0xturboblitz <florent.tavernier@gmail.com>
2023-02-10 11:50:45 +0100

    fix chart color in dark mode

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,5 +1,6 @@
 import { LineChart } from "react-native-wagmi-charts";
-import { Dimensions } from "react-native";
+import { Dimensions, useColorScheme } from "react-native";
+import { colors } from "../config/configs";
 const screenWidth = Dimensions.get("window").width;
 
 export type Point = {
@@ -8,10 +9,13 @@ export type Point = {
 };
 
 function Chart({ chart }: { chart: Point[] }) {
+  const colorScheme = useColorScheme();
   return (
     <LineChart.Provider data={chart}>
       <LineChart width={(screenWidth * 11) / 12} height={150} yGutter={16}>
-        <LineChart.Path />
+        <LineChart.Path
+          color={colorScheme === "light" ? colors.typo.light : colors.typo.dark}
+        />
         <LineChart.CursorCrosshair />
       </LineChart>
     </LineChart.Provider>
