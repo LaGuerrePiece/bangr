280c9283ea86d571af14b32e8b02cb2e0ed16ec1
0xturboblitz <florent.tavernier@gmail.com>
2023-03-20 22:33:17 +0100

    started messing around

--- src/screens/onramp/Monerium.tsx
+++ src/screens/onramp/Monerium.tsx
@@ -14,32 +14,57 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import CryptoJS from "crypto-js";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { TextInput } from "react-native-gesture-handler";
+import { colors } from "../../config/configs";
+import ActionButton from "../../components/ActionButton";
+
+type UserData = {
+  email?: string;
+  password?: string;
+  country?: string;
+  userId?: string;
+  emailCode?: string;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+  address?: string;
+  city?: string;
+  zip?: string;
+};
 
 export default function Monerium() {
+  const colorScheme = useColorScheme();
   const windowWidth = Dimensions.get("window").width;
   const { smartWalletAddress, userInfo } = useUserStore((state) => ({
     smartWalletAddress: state.smartWalletAddress,
     userInfo: state.userInfo,
   }));
 
+  const [step, setStep] = useState<string>("welcome");
   const [codeVerifier, setCodeVerifier] = useState<string>("");
-  const [webWiewUri, setWebWiewUri] = useState<string>("");
+  const [codeChallenge, setCodeChallenge] = useState<string>("");
+  const [userData, setUserData] = useState<UserData>({});
+
+  useEffect(() => {
+    authenticateAsync();
+  }, []);
 
   const authenticateAsync = async () => {
     if (codeVerifier) return;
     const verifier = ethers.utils.randomBytes(64).toString();
-    setCodeVerifier(verifier);
-    const codeChallenge = CryptoJS.enc.Base64url.stringify(
+    const challenge = CryptoJS.enc.Base64url.stringify(
       CryptoJS.SHA256(verifier)
     );
+    setCodeVerifier(verifier);
+    setCodeChallenge(challenge);
 
     console.log("verifier", verifier);
-    console.log("codeChallenge", codeChallenge);
+    console.log("challenge", challenge);
 
     const params = {
       client_id: "fe7e8ccb-ad2d-11ed-97a8-f2eccd865638",
       redirect_uri: "https://www.youtube.com/",
-      code_challenge: codeChallenge,
+      code_challenge: challenge,
       code_challenge_method: "S256",
       // automate the wallet connect step by adding the following optional parameters
       // address: "0xE6E4b6a802F2e0aeE5676f6010e0AF5C9CDd0a50",
@@ -52,8 +77,11 @@ export default function Monerium() {
       `https://api.monerium.app/auth?${new URLSearchParams(params).toString()}`
     );
     console.log("res.url", res.url);
-    setWebWiewUri(res.url);
 
+    // https://monerium.app/partners/012bd5f8-ad2e-11ed-97a8-f2eccd865638/auth?redirect_uri=https%3A%2F%2Fwww.youtube.com%2F%26code_challenge%3DSnwHNPP8EI2XfLDGlxfWsJvvCaSWDcZsFDvHANh0mCo%26code_challenge_method%3DS256%26partner%3D012bd5f8-ad2e-11ed-97a8-f2eccd865638&token=MDI4MDY5
+  };
+
+  const addFirstDetails = async () => {
     const resAfterFirstScreen = await fetch(
       `https://monerium.app/api/iam/signup`,
       {
@@ -63,39 +91,140 @@ export default function Monerium() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username: "yarara5505@oniecan.com",
-          email: "yarara5605@oniecan.com",
-          password: "AZEaze123123!",
+          username: userData.email,
+          email: userData.email,
+          password: userData.password,
           terms: true,
           privacy: true,
-          country: "FR",
-          redirectUri:
-            "https://www.youtube.com/&code_challenge=SnwHNPP8EI2XfLDGlxfWsJvvCaSWDcZsFDvHANh0mCo&code_challenge_method=S256&partner=012bd5f8-ad2e-11ed-97a8-f2eccd865638",
+          country: userData.country,
+          redirectUri: `https://www.youtube.com/&code_challenge=${codeChallenge}&code_challenge_method=S256&partner=012bd5f8-ad2e-11ed-97a8-f2eccd865638`,
           partner: "012bd5f8-ad2e-11ed-97a8-f2eccd865638",
         }),
       }
     );
-    console.log("resAfterFirstScreen json", await resAfterFirstScreen.json());
-
-  
-    // https://monerium.app/partners/012bd5f8-ad2e-11ed-97a8-f2eccd865638/auth?redirect_uri=https%3A%2F%2Fwww.youtube.com%2F%26code_challenge%3DSnwHNPP8EI2XfLDGlxfWsJvvCaSWDcZsFDvHANh0mCo%26code_challenge_method%3DS256%26partner%3D012bd5f8-ad2e-11ed-97a8-f2eccd865638&token=MDI4MDY5
-
+    const json = await resAfterFirstScreen.json();
+    console.log("resAfterFirstScreen json", json);
+    setUserData({
+      ...userData,
+      userId: json.userId,
+    });
   };
 
-  useEffect(() => {
-    authenticateAsync();
-  }, []);
+  const verifyEmailCode = async () => {
+    const wordArray = CryptoJS.enc.Utf8.parse(userData.emailCode!);
+    const base64String = wordArray.toString(CryptoJS.enc.Base64);
+    console.log("base64String", base64String);
+
+    const resAfterEmailCode = await fetch(
+      `https://monerium.app/api/iam/confirm/${base64String}}`
+    );
+    const json = await resAfterEmailCode.json();
+    if (json.auth.verified) {
+      console.log("success");
+    }
+  };
 
   // poche://web3auth
   // exp://192.168.1.22:19000/--/web3auth
 
-  return (
+  return step === "welcome" ? (
     <SafeAreaView className="h-full w-full bg-primary-light dark:bg-primary-dark">
-      {webWiewUri === "" ? (
-        <Text>Loading...</Text>
-      ) : (
-        <WebView style={{ width: windowWidth }} source={{ uri: webWiewUri }} />
-      )}
+      <View className="mx-auto mt-6 w-64 rounded-xl border-2 bg-primary-light p-1 dark:bg-primary-dark">
+        <TextInput
+          style={{
+            color:
+              colorScheme === "light" ? colors.typo.light : colors.typo.dark,
+          }}
+          placeholderTextColor={colors.typo2.light}
+          className="my-1 text-lg font-semibold text-typo-light dark:text-typo-dark"
+          onChangeText={(value) =>
+            setUserData({
+              ...userData,
+              email: value,
+            })
+          }
+          value={userData.email}
+          placeholder="Your email"
+        />
+      </View>
+      <View className="mx-auto mt-6 w-64 rounded-xl border-2 bg-primary-light p-1 dark:bg-primary-dark">
+        <TextInput
+          style={{
+            color:
+              colorScheme === "light" ? colors.typo.light : colors.typo.dark,
+          }}
+          placeholderTextColor={colors.typo2.light}
+          className="my-1 text-lg font-semibold text-typo-light dark:text-typo-dark"
+          onChangeText={(value) =>
+            setUserData({
+              ...userData,
+              password: value,
+            })
+          }
+          value={userData.password}
+          placeholder="Your password"
+        />
+      </View>
+      <View className="mx-auto mt-6 w-64 rounded-xl border-2 bg-primary-light p-1 dark:bg-primary-dark">
+        <TextInput
+          style={{
+            color:
+              colorScheme === "light" ? colors.typo.light : colors.typo.dark,
+          }}
+          placeholderTextColor={colors.typo2.light}
+          className="my-1 text-lg font-semibold text-typo-light dark:text-typo-dark"
+          onChangeText={(value) =>
+            setUserData({
+              ...userData,
+              country: value,
+            })
+          }
+          value={userData.country}
+          placeholder="Your country (FR for France)"
+        />
+      </View>
+      <ActionButton
+        text="Next"
+        action={() => {
+          addFirstDetails();
+          setStep("second");
+        }}
+      />
     </SafeAreaView>
-  );
+  ) : step === "second" ? (
+    <SafeAreaView className="h-full w-full bg-primary-light dark:bg-primary-dark">
+      <Text className="mt-6 text-center text-2xl font-bold">
+        Sent you an email ! Code ? :
+      </Text>
+      <View className="mx-auto mt-6 w-64 rounded-xl border-2 bg-primary-light p-1 dark:bg-primary-dark">
+        <TextInput
+          style={{
+            color:
+              colorScheme === "light" ? colors.typo.light : colors.typo.dark,
+          }}
+          placeholderTextColor={colors.typo2.light}
+          className="my-1 text-lg font-semibold text-typo-light dark:text-typo-dark"
+          onChangeText={(value) =>
+            setUserData({
+              ...userData,
+              emailCode: value,
+            })
+          }
+          value={userData.emailCode}
+          placeholder="6 digits code"
+        />
+      </View>
+      <ActionButton
+        text="Next"
+        action={() => {
+          verifyEmailCode();
+          setStep("third");
+        }}
+      />
+    </SafeAreaView>
+  ) : step === "third" ? (
+    <SafeAreaView className="h-full w-full bg-primary-light dark:bg-primary-dark">
+      <Text className="mt-6 text-center text-2xl font-bold">Third</Text>
+    </SafeAreaView>
+  ) : null;
 }
