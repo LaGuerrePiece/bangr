0b6c9493f5346ebffa62ab240f6b301dfb7dfc89
0xturboblitz <florent.tavernier@gmail.com>
2023-03-25 19:34:18 +0100

    server url in utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -280,12 +280,9 @@ export function getChainWithMaxBalance(chains: MultichainToken["chains"]) {
 }
 
 export const getURLInApp = () =>
-  // process.env.NODE_ENV == "development"
-    // ? `http://${Constants.manifest?.debuggerHost?.split(":").shift()}:3000`
-    // : "https://dev.poche.fi";
-    // "https://dev.poche.fi";
-    "http://192.168.1.122:3000/";
-
+  process.env.NODE_ENV == "development"
+    ? `http://${Constants.manifest?.debuggerHost?.split(":").shift()}:3000`
+    : "https://dev.poche.fi";
 
 export const correctInput = (input: string): string => {
   return input.replace(/,/g, ".");
