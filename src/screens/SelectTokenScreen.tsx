70b7644faf9eed417806adbb5947cd6d19300a90
0xturboblitz <florent.tavernier@gmail.com>
2023-04-07 15:45:08 +0200

    SelectTokenScreen scroll fix

--- src/screens/SelectTokenScreen.tsx
+++ src/screens/SelectTokenScreen.tsx
@@ -35,8 +35,8 @@ export default function SelectToken() {
   const colorScheme = useColorScheme();
 
   return (
-    <SafeAreaView className="mx-auto h-full w-full bg-secondary-light p-3 dark:bg-secondary-dark">
-      <View className="my-6">
+    <SafeAreaView className="h-full w-full bg-secondary-light dark:bg-secondary-dark">
+      <View className="my-6 ml-4">
         <TouchableWithoutFeedback onPress={navigation.goBack}>
           <XMarkIcon
             size={36}
@@ -46,41 +46,46 @@ export default function SelectToken() {
           />
         </TouchableWithoutFeedback>
       </View>
-      <ScrollView className="mx-auto w-11/12">
-        {tokenList.map((token, i) => {
-          return (
-            <TouchableOpacity
-              key={i}
-              className="mx-2 my-1 flex cursor-pointer flex-row items-center justify-between rounded-md border p-2 dark:border-typo-dark"
-              onPress={() => {
-                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
-                tokenToUpdate === "Swap:srcToken"
-                  ? updateSrcToken(token)
-                  : tokenToUpdate === "Swap:dstToken"
-                  ? updateDstToken(token)
-                  : tokenToUpdate === "Send"
-                  ? updateSendToken(token)
-                  : null;
-                navigation.goBack();
-              }}
-            >
-              <View className="flex flex-row items-center">
-                <Image className="h-7 w-7" source={{ uri: token.logoURI }} />
-                <View className="mx-3 flex flex-col">
-                  <Text className="text-typo-light dark:text-typo-dark">
-                    {token.name}
-                  </Text>
-                  <Text className="text-sm text-typo-light dark:text-typo-dark">
-                    {token.symbol}
-                  </Text>
+      <ScrollView>
+        <View className="mx-auto h-full w-11/12 p-3">
+          {tokenList.map((token, i) => {
+            return (
+              <TouchableOpacity
+                key={i}
+                className="mx-2 my-1 flex cursor-pointer flex-row items-center justify-between rounded-md border p-2 dark:border-typo-dark"
+                onPress={() => {
+                  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+                  tokenToUpdate === "Swap:srcToken"
+                    ? updateSrcToken(token)
+                    : tokenToUpdate === "Swap:dstToken"
+                    ? updateDstToken(token)
+                    : tokenToUpdate === "Send"
+                    ? updateSendToken(token)
+                    : null;
+                  navigation.goBack();
+                }}
+              >
+                <View className="flex flex-row items-center">
+                  <Image
+                    className="h-7 w-7 rounded-full"
+                    source={{ uri: token.logoURI }}
+                  />
+                  <View className="mx-3 flex flex-col">
+                    <Text className="text-typo-light dark:text-typo-dark">
+                      {token.name}
+                    </Text>
+                    <Text className="text-sm text-typo-light dark:text-typo-dark">
+                      {token.symbol}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-              <Text className="text-typo-light dark:text-typo-dark">
-                {formatUnits(token.balance, token.decimals, 5)}
-              </Text>
-            </TouchableOpacity>
-          );
-        })}
+                <Text className="text-typo-light dark:text-typo-dark">
+                  {formatUnits(token.balance, token.decimals, 5)}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
