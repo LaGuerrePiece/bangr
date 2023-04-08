0ce9d7b86ab6c009919f589a91f813208e59e092
nicodlz <ndlz@pm.me>
2023-04-08 18:38:22 +0200

    add supabase for oAuth

--- src/screens/onboard/supabase.tsx
+++ src/screens/onboard/supabase.tsx
@@ -1,6 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
-import { makeRedirectUri, startAsync } from 'expo-auth-session';
 
 export const supabaseUrl = "https://gfdakuvgcmtzsngfshnb.supabase.co"
 export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZGFrdXZnY210enNuZ2ZzaG5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI4MjkxNjQsImV4cCI6MTk4ODQwNTE2NH0.W74ATfMAhxpxjc4pO_44L4Q5YiPYWKsNA80DoIuwCqQ"
