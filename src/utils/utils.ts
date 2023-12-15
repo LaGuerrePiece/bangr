e5965a5b9a1c79aefe108c96f5c01bf6671cfcdf
0xturboblitz <florent.tavernier@gmail.com>
2023-12-15 16:34:24 -0500

    fixed server url

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -269,7 +269,7 @@ export function getChainWithMaxBalance(chains: MultichainToken["chains"]) {
 export const getURLInApp = () =>
   process.env.NODE_ENV == "development"
     ? `http://${Constants.manifest?.debuggerHost?.split(":").shift()}:3000`
-    : "https://dev.poche.fi";
+    : "https://poche-git-dev-nicodlz-s-team.vercel.app";
   // "https://dev.poche.fi";
 
 export const correctInput = (input: string): string => {
